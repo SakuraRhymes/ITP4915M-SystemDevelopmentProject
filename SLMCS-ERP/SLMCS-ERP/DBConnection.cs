a53f9f38d0862dd590d5c9d455890bec671176b2
billjjjj <novicetown@gmail.com>
2019-05-21 00:12:46 +0800

    connection OK

--- SLMCS-ERP/SLMCS-ERP/DBConnection.cs
+++ SLMCS-ERP/SLMCS-ERP/DBConnection.cs
@@ -1,28 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace SLMCS_ERP
 {
     class DBConnection
     {
-        private string dbHost = "";//host address
-        private string dbPort = "";//host port
-        private string dbUser = "";//database login id
-        private string dbPassword = "";//database login password
-        private string dbName = "";//database name
-        private MySqlConnection connection;
+        private string dbHost;//host address
+        private string dbPort;//host port
+        private string dbUser;//database login id
+        private string dbPassword;//database login password
+        private string dbName;//database name
+        private MySqlConnection connection;//mysql object
 
-        public DBConnection()
+        public DBConnection() : this("billjjjj.myds.me", "3307", "test", "2121", "sql_demo") { }
+  
+        public DBConnection(string dbHost, string dbPort, string dbUser, string dbPassword, string dbName)
         {
+            this.dbHost = dbHost;
+            this.dbPort = dbPort;
+            this.dbUser = dbUser;
+            this.dbPassword = dbPassword;
+            this.dbName = dbName;
+
             string connString = "server=" + dbHost + ";port=" + dbPort + ";uid=" + dbUser + ";pwd=" + dbPassword + ";database=" + dbName;
             connection = new MySqlConnection(connString);
         }
-
+        //open database connection
         public bool openConnection()
         {
             try
@@ -30,13 +34,12 @@ namespace SLMCS_ERP
                 connection.Open();
                 return true;
             }
-            catch (MySqlException e)
+            catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return false;
+                throw e;
             }
         }
-
+        //close database connection
         public bool closeConnection()
         {
             try
@@ -44,13 +47,12 @@ namespace SLMCS_ERP
                 connection.Close();
                 return true;
             }
-            catch (MySqlException e)
+            catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return false;
+                throw e;
             }
         }
-
+        //get data from table
         public DataTable getDataTable(string selectQuery)
         {
             DataTable dataTable = new DataTable();
@@ -66,6 +68,99 @@ namespace SLMCS_ERP
                 return dataTable;
             }
             return dataTable;
+        }
+        //insert new data to database by short sql statement
+        public void insert(string table, string values)
+        {
+            try
+            {
+                openConnection();
+                string selectQuery = "INSERT INTO " + table + " VALUES (" + values + ")";
+                MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        //insert new data to database by full sql statement
+        public void insert(string insertQuery)
+        {
+            try
+            {
+                openConnection();
+                MySqlCommand cmd = new MySqlCommand(insertQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        //update data from database by short sql statement
+        public void update(string table, string field, string condition)
+        {
+            try
+            {
+                openConnection();
+                string updateQuery = "UPDATE " + table + " SET " + field + " " + condition;
+                MySqlCommand cmd = new MySqlCommand(updateQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        //update data from database by full sql statement
+        public void update(string updateQuery)
+        {
+            try
+            {
+                openConnection();
+                MySqlCommand cmd = new MySqlCommand(updateQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        //delete data from database by short sql statement
+        public void delete(string table, string condition)
+        {
+            try
+            {
+                openConnection();
+                string deleteQuery = "DELETE FROM " + table + " " + condition;
+                MySqlCommand cmd = new MySqlCommand(deleteQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        //delete data from database by full sql statement
+        public void delete(string deleteQuery)
+        {
+            try
+            {
+                openConnection();
+                MySqlCommand cmd = new MySqlCommand(deleteQuery, connection);
+                cmd.ExecuteNonQuery();
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
         }
     }
 }
---
+++ SLMCS-ERP/SLMCS-ERP/TestConnection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SLMCS_ERP
+{
+    public partial class TestConnection : Form
+    {
+        DBConnection mysqlcon = new DBConnection();
+        public TestConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                mysqlcon.openConnection();
+                MessageBox.Show("OK");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = mysqlcon.getDataTable("SELECT * FROM testTable");
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               // mysqlcon.insert("testTable", "2, 'Peter'");
+                mysqlcon.insert("INSERT INTO testTable VALUES (3,'KKK')");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mysqlcon.update("UPDATE testTable SET name='OKOK'");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mysqlcon.delete("DELETE FROM testTable");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
