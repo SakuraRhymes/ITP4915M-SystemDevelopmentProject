e903db07ad0af8d04a9ff91325cc36da92de7c98
SakuraRhymes <sakurarhymes@gmail.com>
2019-05-21 13:54:41 +0800

    added Main

---
+++ SLMCS-ERP/SLMCS-ERP/frmHome.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SLMCS_ERP
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLeftNavigate = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panelLeftNavigate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLeftNavigate
+            // 
+            this.panelLeftNavigate.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelLeftNavigate.Controls.Add(this.button4);
+            this.panelLeftNavigate.Controls.Add(this.button3);
+            this.panelLeftNavigate.Controls.Add(this.button2);
+            this.panelLeftNavigate.Controls.Add(this.button1);
+            this.panelLeftNavigate.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftNavigate.Location = new System.Drawing.Point(0, 0);
+            this.panelLeftNavigate.Name = "panelLeftNavigate";
+            this.panelLeftNavigate.Size = new System.Drawing.Size(200, 708);
+            this.panelLeftNavigate.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 54);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(36, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 54);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 708);
+            this.Controls.Add(this.panelLeftNavigate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmHome";
+            this.Text = "Home";
+            this.panelLeftNavigate.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeftNavigate;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
