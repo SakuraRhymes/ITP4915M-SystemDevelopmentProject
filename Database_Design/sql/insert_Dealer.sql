15f098782319b7d061dc8edacd9b80d6d95bc03a
billjjjj <novicetown@gmail.com>
2019-05-21 23:29:18 +0800

    DB

---
+++ Database_Design/sql/insert_Dealer.sql
@@ -0,0 +1,61 @@
+﻿SET NAMES 'utf8';
+USE SLMCS_DB;
+--
+-- Delete data from the table 'Dealer'
+--
+DELETE FROM Dealer;
+
+--
+-- Inserting data into table Dealer
+--
+INSERT INTO Dealer(DealerID, DealerName, DealerShippingAddress, DealerPhoneNo) VALUES
+('D0000000', 'Letha', '240 Hidden Front Way', '80460192228'),
+('D0000001', 'Kraig', '2948 Stonewood Pkwy', '68490827623'),
+('D0000002', 'Harlan', '44 West Church Ct', '00368767478'),
+('D0000003', 'Brad', '3778 Edgewood Highway', '64449631587'),
+('D0000004', 'Enrique', '12 1st Circle', '84009375351'),
+('D0000005', 'Bennie', '3570 Flintwood Parkway', '34115891693'),
+('D0000006', 'Francis', '1790 SW Church Avenue', '79570539869'),
+('D0000007', 'Nickie', '2014 Social Road', '53293261764'),
+('D0000008', 'Boyd', '34 Riddle Hill Pkwy', '57335893201'),
+('D0000009', 'Alonzo', '65 Waterview Avenue', '48823116874'),
+('D0000010', 'Reed', '3760 West Social Parkway', '01414516278'),
+('D0000011', 'Cory', '1254 New Cedar Tree Rd', '22183842355'),
+('D0000012', 'Abbie', '2354 N Cedar Tree Hwy', '62976086964'),
+('D0000013', 'Lazaro', '95 North Church Road', '65249368582'),
+('D0000014', 'Danielle', '47 W Ashwood Avenue', '04878184404'),
+('D0000015', 'Gregorio', '2455 Buttonwood Ln', '62008942994'),
+('D0000016', 'Monty', '10 Social Lane', '74266355509'),
+('D0000017', 'Abby', '633 New Parkwood St', '89203167930'),
+('D0000018', 'Evelin', '2762 Buttonwood Road', '63438151769'),
+('D0000019', 'Earline', '1027 NW Ashwood Cir', '27007622225'),
+('D0000020', 'Viviana', '38 South Mount Highway', '99070628884'),
+('D0000021', 'Chae', '2795 East Lake Ct', '50437058002'),
+('D0000022', 'Delta', '1063 SW Hope Pkwy', '36884763047'),
+('D0000023', 'Toney', '3172 Deepwood Blvd', '15944328100'),
+('D0000024', 'Wynell', '582 Rushwood Court', '27868892048'),
+('D0000025', 'Ashley', '994 SW Glenwood Parkway', '24810650561'),
+('D0000026', 'Ressie', '1136 SW Ashwood St', '41300364487'),
+('D0000027', 'Shane', '36 NE Hunting Hill Way', '98175045510'),
+('D0000028', 'Raleigh', '105 Old Deepwood Lane', '63294076195'),
+('D0000029', 'Jean', '777 Pine Tree Pkwy', '41724714178'),
+('D0000030', 'Roosevelt', '75 Riverside Rd', '44786222330'),
+('D0000031', 'Alanna', '3474 New Farmview St', '03902968983'),
+('D0000032', 'Lilliana', '255 Edgewood Street', '73937286219'),
+('D0000033', 'Amiee', '71 Buttonwood Way', '40421088056'),
+('D0000034', 'Rene', '2049 North Chapel Hill Street', '59987909086'),
+('D0000035', 'Demarcus', '81 Old Mountain Ln', '03823586550'),
+('D0000036', 'Ashley', '476 Rushwood Pkwy', '18343228666'),
+('D0000037', 'Abel', '139 Mount Hwy', '90933765765'),
+('D0000038', 'Ali', '1296 Beachwood Way', '41095746857'),
+('D0000039', 'Clark', '2052 Mountain Lane', '85547482635'),
+('D0000040', 'Adam', '2429 Rock Hill Highway', '86309795357'),
+('D0000041', 'Gayle', '2402 East Rockwood Pkwy', '14621640212'),
+('D0000042', 'Kristy', '685 Rock Hill Loop', '50949104899'),
+('D0000043', 'Ryan', '10 North Highland Blvd', '36874051522'),
+('D0000044', 'Marjory', '701 Ski Hill Drive', '97679513520'),
+('D0000045', 'Christopher', '2744 New Rock Hill Way', '14620830031'),
+('D0000046', 'Abraham', '665 SW Waterview Court', '86033027147'),
+('D0000047', 'Lavelle', '11 Rock Hill Hwy', '82066910841'),
+('D0000048', 'Brigida', '802 Oak Blvd', '00341090574'),
+('D0000049', 'Cristobal', '2035 Mountain Pkwy', '99704973998');
---
+++ Database_Design/sql/insert_Vendor.sql
@@ -0,0 +1,61 @@
+SET NAMES 'utf8';
+USE SLMCS_DB;
+--
+-- Delete data from the table 'Vendor'
+--
+DELETE FROM Vendor;
+
+--
+-- Inserting data into table Vendor
+--
+INSERT INTO Vendor(VendorID, VendorName, VendorAddress, VendorPhoneNo) VALUES
+('V0000000', 'Smart H-Mobile Co.', '1388 Parkwood Loop, STE 31, Lansing, Michigan, 24438', '91747636842'),
+('V0000001', 'Federal Space Research Group', '2083 SE Bayview Hwy, Diamond Bldg, Austin, Texas, 35512', '43047518570'),
+('V0000002', 'WorldWide Partners Corporation', '3782 Hidden Meadowview Avenue, Comcast Bldg, Phoenix, AZ, 14366', '28383782178'),
+('V0000003', 'Special Engineering Group', '56 Front Way, 7th Floor, Salem, Oregon, 66021', '42842812943'),
+('V0000004', 'General B-Mobile Corp.', '646 SE Ironwood Avenue, Little Rock, Arkansas, 83249', '42252147580'),
+('V0000005', 'Professional Wind Energy Inc.', '1086 Brentwood Blvd, Dover, DE, 59369', '77185159659'),
+('V0000006', 'Home Protection Corporation', '94 North Chapel Hill Pkwy, APT 436, Frankfort, Kentucky, 94314', '33188101585'),
+('V0000007', 'Home High-Technologies Group', '3543 Rockwood Way, Bartlett Building, Santa Fe, NM, 74100', '24748895581'),
+('V0000008', 'Pacific O-Mobile Group', '296 SW Rockwood Blvd, Comcast Building, Saint Paul, Minnesota, 63918', '72117475734'),
+('V0000009', 'North Space Research Inc.', '291 E Market Pkwy, Helena, MT, 32277', '86011472058'),
+('V0000010', 'Union High-Technologies Corp.', '1811 S Rock Hill Blvd, Juneau, AK, 04145', '76060293998'),
+('V0000011', 'Union Trust Inc.', '2676 E Prospect Hill Ave, Lincoln, NE, 15194', '24465157333'),
+('V0000012', 'Professional Business Co.', '605 White Rock Hill Lane, 86th Floor, Montpelier, VT, 67021', '67153468540'),
+('V0000013', 'East Space Explore Corp.', '441 S Ski Hill Highway, Bartlett Building, Boise, Idaho, 94526', '19872343962'),
+('V0000014', 'First High-Technologies Inc.', '2526 West Pine Tree Highway, Diamond Building, Salt Lake City, Utah, 01418', '52673529967'),
+('V0000015', 'North Telecom Corp.', '2815 W Ashwood Pkwy, 9th Floor, Indianapolis, IN, 35378', '86506472646'),
+('V0000016', 'South Travel Inc.', '653 NE Edgewood St, Columbia, South Carolina, 96021', '95730291571'),
+('V0000017', 'Pacific High-Technologies Group', '75 Town Ln, Frankfort, Kentucky, 24833', '99425478779'),
+('V0000018', 'Canadian High-Technologies Inc.', '694 Hidden Ironwood Loop, Standard Bldg, Denver, CO, 06597', '78095305596'),
+('V0000019', 'WorldWide M-Mobile Co.', '1547 NE Ski Hill Dr, Kearns Building, Jackson, Mississippi, 91926', '28161132614'),
+('V0000020', 'Special High-Technologies Corporation', '1858 Waterview Ln, Appartment 67, Topeka, Kansas, 31062', '09833375070'),
+('V0000021', 'First Systems Corporation', '2130 White Mountain Street, First Security Bldg, Indianapolis, Indiana, 45948', '68524316845'),
+('V0000022', 'International High-Technologies Corp.', '3250 East Riverside Road, Phoenix, AZ, 34596', '75997379950'),
+('V0000023', 'Creative Explore Corporation', '1179 North Mountain Hwy, Des Moines, Iowa, 27520', '44248497795'),
+('V0000024', 'City 2G Wireless Co.', '1964 NE Rose Hill Lane, Columbia, South Carolina, 97413', '74485767134'),
+('V0000025', 'Creative Research Corp.', '422 Fox Hill Hwy, Suite 515, Lincoln, Nebraska, 90113', '43342961999'),
+('V0000026', 'Canadian Space Explore Corp.', '3265 Market Lane, Dover, Delaware, 26858', '89194021861'),
+('V0000027', 'Future High-Technologies Co.', '58 E Quailwood Ct, Diamond Bldg, Tallahassee, Florida, 10323', '74691511599'),
+('V0000028', 'International Industry Inc.', '969 Burwood Court, Appartment 47, Sacramento, California, 04454', '48137604219'),
+('V0000029', 'West Coast I-Mobile Group', '67 Hidden Lake Blvd, Diamond Bldg, Raleigh, NC, 20451', '26347869131'),
+('V0000030', 'Western Data Corp.', '316 Church Blvd, Juneau, AK, 41935', '00911352594'),
+('V0000031', 'American Travel Corp.', '758 Prospect Hill Dr, 52th FL, Annapolis, Maryland, 40674', '80799346358'),
+('V0000032', 'National Data Corp.', '31 S Stonewood Loop, STE 87, Juneau, Alaska, 53219', '52370340629'),
+('V0000033', 'American Explore Co.', '63 Pine Tree Ave, Judge Building, Lansing, Michigan, 06118', '72069113054'),
+('V0000034', 'National High-Technologies Corporation', '319 Prospect Hill Blvd, Keith Bldg, Boise, Idaho, 76994', '21699750095'),
+('V0000035', 'Domestic Space Research Inc.', '238 Hunting Hill Way, Honolulu, Hawaii, 19687', '91627092132'),
+('V0000036', 'Smart Services Group', '61 Ironwood Parkway, Plaza Bldg, Baton Rouge, Louisiana, 38502', '43226595043'),
+('V0000037', 'Future S-Mobile Corp.', '1761 Front Lane, Salt Lake City, Utah, 70320', '99430042999'),
+('V0000038', 'Special Space Explore Corp.', '2513 SW Pine Tree Parkway, 3rd FL, Tallahassee, FL, 28146', '41097341979'),
+('V0000039', 'Future High-Technologies Corporation', '122 White Edgewood Ave, Atlanta, Georgia, 14003', '34076684677'),
+('V0000040', 'Global Devices Corporation', '2619 New Flintwood Hwy, Boise, ID, 31658', '42700685330'),
+('V0000041', 'Western Petroleum Co.', '2158 South Hazelwood Highway, Duke Energy Building, Hartford, Connecticut, 46929', '81931372041'),
+('V0000042', 'National Mobile Inc.', '1503 Market Cir, Concord, NH, 29993', '88080443714'),
+('V0000043', 'Advanced E-Mobile Inc.', '1495 Rose Hill Rd, Guardian Building, Salem, OR, 07561', '31423982675'),
+('V0000044', 'Australian High-Technologies Corp.', '2094 N Pine Tree Circle, Frankfort, Kentucky, 59405', '91845685640'),
+('V0000045', 'East Green Resources Group', '1959 Mount Cir, Diamond Bldg, Santa Fe, New Mexico, 09699', '82907239771'),
+('V0000046', 'North Nuclear Resources Inc.', '3001 West Ski Hill Way, Buhl Building, Topeka, Kansas, 91545', '80249455504'),
+('V0000047', 'North Software Corporation', '2340 Lake Hwy, Montgomery, AL, 99427', '21056018305'),
+('V0000048', 'Creative 3G Wireless Group', '55 Rockwood Parkway, Plaza Bldg, Salt Lake City, Utah, 30352', '43397300880'),
+('V0000049', 'Beyond High-Technologies Co.', '3026 North Sharp Hill Ln, Madison, Wisconsin, 96478', '03595294593');
