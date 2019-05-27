fcd27b0d7884ca1b5a7da7c3ea4a2d446cd4dc76
toker <44666673+poi9079@users.noreply.github.com>
2019-05-27 17:08:16 +0800

    Base

---
+++ SLMCS-ERP/SLMCS-ERP/UI/Dispatch/frmDispatchMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SLMCS_ERP
+{
+    partial class frmDispatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelContect = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 566);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelContect
+            // 
+            this.panelContect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContect.Location = new System.Drawing.Point(150, 0);
+            this.panelContect.Name = "panelContect";
+            this.panelContect.Size = new System.Drawing.Size(1036, 566);
+            this.panelContect.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "View Dispatching Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Goods Received ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // frmDispatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 566);
+            this.Controls.Add(this.panelContect);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDispatch";
+            this.Text = "frmDispatchMain";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeftNavigate;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSalesOrderRecords;
+        private System.Windows.Forms.Button btnNewSalesOrder;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelContect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ SLMCS-ERP/SLMCS-ERP/UI/Dispatch/frmDispatchMain.cs
@@ -0,0 +1,32 @@
+﻿//using SLMCS_ERP.UI.Dispatch;
+using System;
+using System.Windows.Forms;
+
+namespace SLMCS_ERP
+{
+    public partial class frmDispatch : Form
+    {
+        public frmDispatch()
+        {
+            InitializeComponent();
+        }
+        private void navigation(Form form, Panel panel)
+        {
+            form.TopLevel = false;
+            panelContent.Controls.Clear();
+            panelContent.Controls.Add(form);
+            form.Show();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
