abca8d2e9f96070a3da15b211a3122072b738e85
toker <44666673+poi9079@users.noreply.github.com>
2019-06-05 13:54:47 +0800

    Nothing here

--- SLMCS-ERP/SLMCS-ERP/UI/Management/frmProductManagement.Designer.cs
+++ SLMCS-ERP/SLMCS-ERP/UI/Management/frmProductManagement.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmProductManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1186, 566);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmProductManagement";
             this.Text = "frmProductManagement";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
