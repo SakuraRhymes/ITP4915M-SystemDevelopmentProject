c3531665e0fd8447465ccf76e7d50bdf9436e01e
toker <44666673+poi9079@users.noreply.github.com>
2019-06-05 18:32:38 +0800

    Revert "Udated dealer(constractor, next ID, searching, create)menth and interface(not done)"
    This reverts commit d777abdb48e0347d01f78d4a33a86245f4a136c9.

--- SLMCS-ERP/SLMCS-ERP/UI/Management/frmDealerManagement.cs
+++ SLMCS-ERP/SLMCS-ERP/UI/Management/frmDealerManagement.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using SLMCS_Class;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,10 +15,6 @@ namespace SLMCS_ERP.UI.Management
         public frmDealerManagement()
         {
             InitializeComponent();
-            dgvDealerList.AllowUserToAddRows = false;
-            dgvDealerList.RowHeadersVisible = false;
-            dgvDealerList.ReadOnly = true;
-            Dealer dealer = new Dealer();
         }
     }
 }
