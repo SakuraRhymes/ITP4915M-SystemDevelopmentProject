1ce8bd579611ba32ebcedac69cffbf6eab14cd0f
billjjjj <novicetown@gmail.com>
2019-06-06 09:43:22 +0800

    Update view.sql

--- Database_Design/view.sql
+++ Database_Design/view.sql
@@ -2,9 +2,9 @@ CREATE VIEW ReorderLevelProduct AS
 SELECT * FROM Product 
 WHERE ActualQuantity <= ReorderLevel 
 AND ActualQuantity > DangerLevel 
-AND ReorderLevel <> 0 
+AND ReorderLevel <> 0 ;
 
 CREATE VIEW DangerLevelProduct AS
 SELECT * FROM Product 
 WHERE ActualQuantity <= DangerLevel 
-AND DangerLevel <> 0 
+AND DangerLevel <> 0 ;
