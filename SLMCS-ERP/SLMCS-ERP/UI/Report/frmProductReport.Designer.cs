c482a16ec103a86a3c28864cc8c61cc9bca576d3
billjjjj <novicetown@gmail.com>
2019-06-26 22:23:45 +0800

    80%

--- SLMCS-ERP/SLMCS-ERP/UI/Report/frmProductReport.Designer.cs
+++ SLMCS-ERP/SLMCS-ERP/UI/Report/frmProductReport.Designer.cs
@@ -28,12 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblTotalSaleQty = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblCompanyName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(600, 155);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series3.Legend = "Legend1";
+            series3.Name = "s1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(407, 399);
+            this.chart1.TabIndex = 29;
+            this.chart1.Text = "chart1";
+            // 
+            // lblTotalSaleQty
+            // 
+            this.lblTotalSaleQty.AutoSize = true;
+            this.lblTotalSaleQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSaleQty.Location = new System.Drawing.Point(427, 194);
+            this.lblTotalSaleQty.Name = "lblTotalSaleQty";
+            this.lblTotalSaleQty.Size = new System.Drawing.Size(30, 20);
+            this.lblTotalSaleQty.TabIndex = 28;
+            this.lblTotalSaleQty.Text = "qty";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(775, 94);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(59, 29);
+            this.lblYear.TabIndex = 27;
+            this.lblYear.Text = "year";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(261, 194);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(107, 20);
+            this.lblProductName.TabIndex = 26;
+            this.lblProductName.Text = "product name";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductID.Location = new System.Drawing.Point(121, 194);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(79, 20);
+            this.lblProductID.TabIndex = 25;
+            this.lblProductID.Text = "product id";
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRank.Location = new System.Drawing.Point(30, 194);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(40, 20);
+            this.lblRank.TabIndex = 24;
+            this.lblRank.Text = "rank";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(135)));
+            this.label6.Location = new System.Drawing.Point(29, 155);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 24);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Rank :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(135)));
+            this.label5.Location = new System.Drawing.Point(427, 155);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 24);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Total Sale Qty :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(688, 94);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 29);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Year  :";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(33, 181);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(522, 1);
+            this.panel1.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(135)));
+            this.label3.Location = new System.Drawing.Point(261, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 24);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Product Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(135)));
+            this.label1.Location = new System.Drawing.Point(121, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 24);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Product ID :";
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.lblHeader.Location = new System.Drawing.Point(324, 94);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(314, 29);
+            this.lblHeader.TabIndex = 17;
+            this.lblHeader.Text = "Top 10 Fast Moving Product";
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompanyName.Location = new System.Drawing.Point(115, 25);
+            this.lblCompanyName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(795, 55);
+            this.lblCompanyName.TabIndex = 16;
+            this.lblCompanyName.Text = "Smart And Luxury Motor Company";
+            // 
+            // frmProductReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1036, 566);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblTotalSaleQty);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.lblRank);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.lblCompanyName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmProductReport";
             this.Text = "frmProductReport";
+            this.Load += new System.EventHandler(this.FrmProductReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lblTotalSaleQty;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.Label lblRank;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblCompanyName;
     }
 }
--- SLMCS-ERP/SLMCS-ERP/UI/Report/frmProductReport.cs
+++ SLMCS-ERP/SLMCS-ERP/UI/Report/frmProductReport.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SLMCS_Class;
 
 namespace SLMCS_ERP
 {
     public partial class frmProductReport : Form
     {
+        private Report report;
+        private DataTable topTenTable;
+        private DataTable topTenTableYear;
         public frmProductReport()
         {
             InitializeComponent();
+            report = new Report();
+        }
+
+        private void FrmProductReport_Load(object sender, EventArgs e)
+        {
+            string rank = "";
+            string productID = "";
+            string productName = "";
+            string totalSaleQty = "";
+
+            chart1.Series["s1"]["PieLabelStyle"] = "Disabled";
+            chart1.Legends[0].Alignment = System.Drawing.StringAlignment.Center;
+
+            //get top 10
+            topTenTable = report.GetTopTenDealerTable();
+            DataRow[] rows = topTenTable.Select();
+            for (int i = 0; i < rows.Length; i++)
+            {
+                rank += (i + 1) + "\n\n";
+                productID += (string)rows[i]["dealerID"] + "\n\n";
+                productName += (string)rows[i]["dealerName"] + "\n\n";
+                totalSaleQty += "$ " + rows[i]["totalAmount"].ToString() + "\n\n";
+                chart1.Series["s1"].Points.AddXY((string)rows[i]["dealerName"], rows[i]["totalAmount"]);
+            }
+            lblRank.Text = rank;
+            lblProductID.Text = productID;
+            lblProductName.Text = productName;
+            lblYear.Text = rows[0]["year"].ToString() + "\n\n";
+            lblTotalSaleQty.Text = totalSaleQty;
+
+            //get year of top 10
+            topTenTableYear = report.GetTopTenDealerTableYear();
+            DataRow[] yearRows = topTenTable.Select();
+            {
+
+            }
         }
     }
 }
